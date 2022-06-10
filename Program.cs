d3e1e7378fbab7f4e8702f344d087add80455f20
awplee <awplee@hotmail.com>
2022-06-11 01:09:11 +0800

    Add Program.cs to the project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ProGitForProgrammers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I just added this in Visual Studio");
+            Console.WriteLine("I just added this in the command line repo");
+            Console.WriteLine("This line added to demonstrate messages.");
+            Console.WriteLine("This line added to show the staging area");
+        }
+    }
+}
